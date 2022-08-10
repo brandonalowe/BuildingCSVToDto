aebaadfadb747e3d5bb584ab0b5c247a65416052
Brandon Lowe <brandon.ldata@gmail.com>
2022-08-11 01:50:35 +1000

    feat: finds buildingtype based on value in csv

--- BuildingCSVToDto/Program.cs
+++ BuildingCSVToDto/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -12,30 +13,69 @@ namespace BuildingCSVtoDto
     {
         static void Main(string[] args)
         {
+            List<CommonCvTypes.Building> toRemove = new List<CommonCvTypes.Building>();
+            List<CommonCvTypes.Building> buildingList = new List<CommonCvTypes.Building>();
+            
             // Set up csv read
-            using var streamReader = new StreamReader("/home/jaan/RiderProjects/BuildingCSVToDto/BuildingCSVToDto/test.csv"); // insert csv dir here yeah?!
+            var file = "building-purchases.csv";
+            using var streamReader = new StreamReader($"/home/jaan/RiderProjects/BuildingCSVToDto/BuildingCSVToDto/{file}"); // insert csv dir here yeah?!
             using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
-            
-            // Context mapping to match csv headers with BuildingDto 
             csvReader.Context.RegisterClassMap<BuildingDtoClassMap>();
-            var buildingList = csvReader.GetRecords<CommonCvTypes.Building>().ToList();
+            var buildingValue = csvReader.GetRecords<BuildingValues>();
             
-            foreach (var building in buildingList)
+            foreach (var x in buildingValue)
             {
-                building.Id = Guid.NewGuid();
-                building.LastFed = DateTime.Now;
+                CommonCvTypes.Building temp = new CommonCvTypes.Building();
+                temp.Owner = x.Owner;
+                temp.Id = Guid.NewGuid();
+                temp.LastFed = DateTime.Now;
+                buildingList.Add(temp);
+                switch (x.Value)
+                {
+                    case "50":
+                        temp.BuildingType = CommonCvTypes.BuildingType.Barracks;
+                        break;
+                    case "100":
+                        temp.BuildingType = CommonCvTypes.BuildingType.CamelStables;
+                        break;
+                    case "200":
+                        temp.BuildingType = CommonCvTypes.BuildingType.MageAcademy;
+                        break;
+                    case "500":
+                        temp.BuildingType = CommonCvTypes.BuildingType.TradingPost;
+                        break;
+                    case "1,250":
+                        temp.BuildingType = CommonCvTypes.BuildingType.Bazaar;
+                        break;
+                    case "2,500":
+                        temp.BuildingType = CommonCvTypes.BuildingType.Bank;
+                        break;
+                    case "5,000":
+                        temp.BuildingType = CommonCvTypes.BuildingType.Castle;
+                        break;
+                    default:
+                        toRemove.Add(temp);
+                        break;
+                }
+            }
+
+            foreach (var badTx in toRemove)
+            {
+                buildingList.Remove(badTx);
             }
         }
-        
         // Insert header names here for value mapping ease -- idk what mitch's csv looks like
-        public class BuildingDtoClassMap : ClassMap<CommonCvTypes.Building>
+        public class BuildingDtoClassMap : ClassMap<BuildingValues>
         {
             public BuildingDtoClassMap()
             {
-                Map(m => m.Owner).Name("owner");
-                Map(m => m.BuildingType).Name("building_type");
+                Map(m => m.Owner).Name("From");
+                Map(m => m.Value).Name("Value");
             }
         }
-
+        public class BuildingValues : CommonCvTypes.Building
+        {
+            public string Value { get; set; }
+        }
     }
 }
